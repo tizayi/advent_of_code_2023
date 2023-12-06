6dadbdea8297b05a246afee8d9343e7f29e8ae3f
tizayi <tizayi0@gmail.com>
2023-12-06 15:36:55 +0000

    day 2 completed

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -35,12 +35,27 @@ fn check_hand(hand: Vec<(&str,&str)>, bag: &HashMap<&str, u32>)-> bool {
    true
 }
 
+
 fn parse_game_number(input: &str)-> IResult<&str,&str> {
     let (rest,(_,game)) = separated_pair(tag("Game"), tag(" "), digit1)(input)?;
     Ok((rest,game))
 }
 
-fn parse_game(input: &str, bag: &HashMap<&str, u32>) -> u32 {
+fn update_cube_map<'a>(cubes:&Vec<(&str,&'a str)>, cube_map: &mut HashMap<&'a str, Option<u32>>){
+    for item in cubes {
+        let num:u32 = item.0.parse().unwrap();
+        match cube_map[&item.1] {
+            None => {cube_map.insert(&item.1,Some(num));},
+            Some(value) => {
+                if value < num {
+                    cube_map.insert(&item.1,Some(num));
+                }
+        }
+        };
+    }
+}
+
+fn parse_game(input: &str, bag: &HashMap<&str, u32>) -> (u32,u32) {
     let ( rest , num)= parse_game_number(input).unwrap();
     let game_number: u32 = num.parse().unwrap();
     let (rest, _) = tag::<&str, &str, Error<_>>(": ")(rest).unwrap();
@@ -50,14 +65,37 @@ fn parse_game(input: &str, bag: &HashMap<&str, u32>) -> u32 {
             check_hand(hand, bag)
         }
     );
+
+    let (_,things) = separated_list0(tag("; "),parse_hand)(rest).unwrap();
     
+    let mut fewest_cubes : HashMap<&str, Option<u32>> =
+    [("blue", None),
+     ("red", None),
+     ("green", None)]
+    .iter().cloned().collect();
+
+    
+    for thing in things{
+        update_cube_map(&thing,&mut fewest_cubes);
+    };
+
+    let mut result2:u32 = 1;
+    for (_ , cube) in fewest_cubes {
+        match cube {
+            None => {},
+            Some(value) => {
+                result2*=value
+            }
+        }
+    }
     let (_,result) = separated_list0(tag("; "),hand_works_parser)(rest).unwrap();
 
     if result.iter().all(|x|*x) {
-        return game_number
+        return (game_number,result2);
     }
-    0
+    (0,result2)
 }
+
 
 
 fn main() {
@@ -76,6 +114,6 @@ fn main() {
      ("green", 13)]
     .iter().cloned().collect();
 
-    let result:u32 = contents.lines().map(|line| parse_game(line,&bag)).sum();
-    println!("result {}",result)
+    let results = contents.lines().map(|line| parse_game(line,&bag)).reduce(|a:(u32,u32),b:(u32,u32)| (a.0+b.0,a.1+b.1));
+    println!("part1 {}, part2 {}",results.unwrap().0,results.unwrap().1);
 }
