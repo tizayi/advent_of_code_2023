54a0c14436fe219e931741fff2c0e187491a7352
tizayi <tizayi0@gmail.com>
2023-12-08 17:35:47 +0000

    almost there

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,116 +1,105 @@
+use nom::{
+    branch::alt, bytes::complete::tag, character::complete::digit1, combinator::map, error::Error,
+    multi::separated_list0, sequence::separated_pair, IResult,
+};
 use std::collections::HashMap;
 use std::env;
 use std::fs;
-use nom::{
-    IResult,
-    sequence::separated_pair,
-    character::complete::{digit1},
-    bytes::complete::{tag},
-    branch::alt,
-    multi::{separated_list0},
-    error::Error,
-    combinator::map
-};
 
-
-fn parse_balls(input: &str) -> IResult<&str, (&str,&str)> {
+fn parse_balls(input: &str) -> IResult<&str, (&str, &str)> {
     Ok(separated_pair(digit1, tag(" "), parse_colour)(input)?)
 }
 
-fn parse_hand(input: &str) -> IResult<&str,Vec<(&str,&str)>> {
-    separated_list0(tag(", "),parse_balls)(input)
+fn parse_hand(input: &str) -> IResult<&str, Vec<(&str, &str)>> {
+    separated_list0(tag(", "), parse_balls)(input)
 }
 
-fn parse_colour(input: &str) -> IResult<&str,&str> {
-    alt((tag("blue"),tag("green"),tag("red")))(input)
+fn parse_colour(input: &str) -> IResult<&str, &str> {
+    alt((tag("blue"), tag("green"), tag("red")))(input)
 }
 
-fn check_hand(hand: Vec<(&str,&str)>, bag: &HashMap<&str, u32>)-> bool {
+fn check_hand(hand: Vec<(&str, &str)>, bag: &HashMap<&str, u32>) -> bool {
     for item in hand {
-        let num:u32 = item.0.parse().unwrap();
+        let num: u32 = item.0.parse().unwrap();
         if num > bag[&item.1] {
-            return false
+            return false;
         }
-    };
-   true
+    }
+    true
 }
 
-fn parse_game_number(input: &str)-> IResult<&str,&str> {
-    let (rest,(_,game)) = separated_pair(tag("Game"), tag(" "), digit1)(input)?;
-    Ok((rest,game))
+fn parse_game_number(input: &str) -> IResult<&str, &str> {
+    let (rest, (_, game)) = separated_pair(tag("Game"), tag(" "), digit1)(input)?;
+    Ok((rest, game))
 }
 
-fn update_cube_map<'a>(cubes:&Vec<(&str,&'a str)>, cube_map: &mut HashMap<&'a str, Option<u32>>){
+fn update_cube_map<'a>(cubes: &Vec<(&str, &'a str)>, cube_map: &mut HashMap<&'a str, Option<u32>>) {
     for item in cubes {
-        let num:u32 = item.0.parse().unwrap();
+        let num: u32 = item.0.parse().unwrap();
         match cube_map[&item.1] {
-            None => {cube_map.insert(&item.1,Some(num));},
+            None => {
+                cube_map.insert(&item.1, Some(num));
+            }
             Some(value) => {
                 if value < num {
-                    cube_map.insert(&item.1,Some(num));
+                    cube_map.insert(&item.1, Some(num));
                 }
-        }
+            }
         };
     }
 }
 
-fn parse_game(input: &str, bag: &HashMap<&str, u32>) -> (u32,u32) {
-    let ( rest , num)= parse_game_number(input).unwrap();
+fn parse_game(input: &str, bag: &HashMap<&str, u32>) -> (u32, u32) {
+    let (rest, num) = parse_game_number(input).unwrap();
     let game_number: u32 = num.parse().unwrap();
     let (rest, _) = tag::<&str, &str, Error<_>>(": ")(rest).unwrap();
-    let hand_works_parser = map(
-        parse_hand,
-        |hand :Vec<(&str,&str)>| {
-            check_hand(hand, bag)
-        }
-    );
+    let hand_works_parser = map(parse_hand, |hand: Vec<(&str, &str)>| check_hand(hand, bag));
 
-    let (_,things) = separated_list0(tag("; "),parse_hand)(rest).unwrap();
-    
-    let mut fewest_cubes : HashMap<&str, Option<u32>> =
-    [("blue", None),
-     ("red", None),
-     ("green", None)]
-    .iter().cloned().collect();
+    let (_, things) = separated_list0(tag("; "), parse_hand)(rest).unwrap();
 
-    
-    for thing in things{
-        update_cube_map(&thing,&mut fewest_cubes);
-    };
+    let mut fewest_cubes: HashMap<&str, Option<u32>> =
+        [("blue", None), ("red", None), ("green", None)]
+            .iter()
+            .cloned()
+            .collect();
 
-    let mut result2:u32 = 1;
-    for (_ , cube) in fewest_cubes {
+    for thing in things {
+        update_cube_map(&thing, &mut fewest_cubes);
+    }
+
+    let mut result2: u32 = 1;
+    for (_, cube) in fewest_cubes {
         match cube {
-            None => {},
-            Some(value) => {
-                result2*=value
-            }
+            None => {}
+            Some(value) => result2 *= value,
         }
     }
-    let (_,result) = separated_list0(tag("; "),hand_works_parser)(rest).unwrap();
+    let (_, result) = separated_list0(tag("; "), hand_works_parser)(rest).unwrap();
 
-    if result.iter().all(|x|*x) {
-        return (game_number,result2);
+    if result.iter().all(|x| *x) {
+        return (game_number, result2);
     }
-    (0,result2)
+    (0, result2)
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path: &str = match args.len() {
         2 => {
-            println!("Finding calibration in {}",args[1]);
+            println!("Finding calibration in {}", args[1]);
             &args[1]
-        },
-        _ => panic!("only one argument is needed")
+        }
+        _ => panic!("only one argument is needed"),
     };
     let contents = fs::read_to_string(file_path).expect("File does not exist");
-    let bag: HashMap<&str, u32> =
-    [("blue", 14),
-     ("red", 12),
-     ("green", 13)]
-    .iter().cloned().collect();
+    let bag: HashMap<&str, u32> = [("blue", 14), ("red", 12), ("green", 13)]
+        .iter()
+        .cloned()
+        .collect();
 
-    let results = contents.lines().map(|line| parse_game(line,&bag)).reduce(|a:(u32,u32),b:(u32,u32)| (a.0+b.0,a.1+b.1));
-    println!("part1 {}, part2 {}",results.unwrap().0,results.unwrap().1);
+    let results = contents
+        .lines()
+        .map(|line| parse_game(line, &bag))
+        .reduce(|a: (u32, u32), b: (u32, u32)| (a.0 + b.0, a.1 + b.1));
+    println!("part1 {}, part2 {}", results.unwrap().0, results.unwrap().1);
 }
--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,36 +1,147 @@
+use nom::branch::alt;
+use nom::bytes::complete::is_a;
+use nom::bytes::complete::is_not;
+use nom::bytes::complete::take_while;
+use nom::character::complete::digit1;
+use nom::combinator::eof;
+use nom::error::Error;
+use nom::multi::many_till;
+use nom::IResult;
 use std::env;
 use std::fs;
-use nom::{
-    IResult
-};
 
-struct PartNumber<'a>{
-    number: &'a str,
-    last: Option<&'a str>,
-    next: Option<&'a str>
+fn parse_dots(input: &str) -> IResult<&str, &str> {
+    is_a::<&str, &str, Error<_>>(".")(input)
 }
 
-fn processs_line(line: &str, last: Option<&str>, next: Option<&str> ) -> u32 {
+fn is_not_digit(c: char) -> bool {
+    !c.is_digit(10)
+}
 
-    let curren_line = String::from(line);
-    println!("{:?}", owned_line);
-    
-    
-    
-    4
+fn parse_symbol(input: &str) -> IResult<&str, &str> {
+    alt((is_not("."), take_while(is_not_digit)))(input)
+}
+
+fn parse_line(input: &str) -> IResult<&str, (Vec<&str>, &str)> {
+    many_till(alt((parse_dots, digit1, parse_symbol)), eof)(input)
+}
+
+fn check_line(line_vec: &Vec<&str>, index_vec: &Vec<usize>) -> bool {
+    let mut counter = 0;
+    let edge_list: Vec<usize> = line_vec
+        .iter()
+        .scan(0, |acc, item| {
+            *acc = *acc + item.len();
+            Some(*acc)
+        })
+        .collect();
+
+    for (item_idx, edge) in edge_list.iter().enumerate() {
+        for index in index_vec {
+            if edge_list[item_idx - 1] < *index && index < edge {
+                let new_string = String::from(line_vec[item_idx]);
+                let char_at_index = new_string.chars().nth(edge - 1).unwrap();
+                if !char_at_index.is_digit(10) && char_at_index != '.' {
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
+
+fn process_line(line: &str, last: Option<&str>, next: Option<&str>) -> u32 {
+    let current = parse_line(line);
+    let (_, (current_vec, _)) = current.unwrap();
+
+    let last_vec: Option<Vec<&str>> = match last {
+        Some(value) => {
+            let (_, (last_vec, _)) = parse_line(value).unwrap();
+            Some(last_vec)
+        }
+        None => None,
+    };
+
+    let next_vec: Option<Vec<&str>> = match next {
+        Some(value) => {
+            let (_, (next_vec, _)) = parse_line(value).unwrap();
+            Some(next_vec)
+        }
+        None => None,
+    };
+    println!("{:?} {:?} {:?}", current_vec, next_vec, last_vec);
+
+    let mut line_total = 0;
+
+    let mut counter: usize = 0;
+
+    for (idx, item) in current_vec.iter().enumerate() {
+        match item.parse::<u32>() {
+            Ok(num) => {
+                let mut need_check: Vec<usize> = Vec::new();
+                for i in counter - 1..counter + 2 {
+                    need_check.push(i)
+                }
+                let symbol_in_last = match &last_vec {
+                    Some(last) => check_line(&last, &need_check),
+                    None => false,
+                };
+
+                let symbol_in_next = match &next_vec {
+                    Some(value) => check_line(&value, &need_check),
+                    None => false,
+                };
+
+                let mut symbol_next_to_number = false;
+
+                match current_vec[idx - 1].chars().last() {
+                    Some(value) => {
+                        if !value.is_digit(10) && value != '.' {
+                            symbol_next_to_number = true
+                        }
+                    }
+                    _ => {}
+                };
+
+                match current_vec[idx + 1].chars().last() {
+                    Some(value) => {
+                        if !value.is_digit(10) && value != '.' {
+                            symbol_next_to_number = true
+                        }
+                    }
+                    _ => {}
+                };
+
+                if symbol_in_last || symbol_in_next || symbol_next_to_number {
+                    line_total += num
+                }
+            }
+            Err(_) => {}
+        };
+
+        counter += item.len()
+    }
+
+    println!("{:?}", line_total);
+    line_total
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path: &str = match args.len() {
         2 => {
-            println!("Finding calibration in {}",args[1]);
+            println!("Finding calibration in {}", args[1]);
             &args[1]
-        },
-        _ => panic!("only one argument is needed")
+        }
+        _ => panic!("only one argument is needed"),
     };
-    let contents = fs::read_to_string(file_path).expect("File does not exist");
-    println!("{:?}", contents);
-    processs_line("467..114..");
+    let contents = fs::read_to_string(file_path)
+        .expect("File does not exist")
 
+    for (idx, string_line) in contents.lines().enumerate() {
+        let last = 
+        process_line(string_line, last, next)
+    }
+
+    let _result2 = process_line("...*......", Some("467..114.."), Some("..35..633."));
 }
